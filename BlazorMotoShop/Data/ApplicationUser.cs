7cd8dfd1bc3eedeb5dd0f2b67909715af58083b4
Jefferson Veiga <gelsvsampa@hotmail.com>
2024-01-19 22:25:54 -0300

    Adicionando funcionalidade de upload de imagens de perfil do usuário

--- BlazorMotoShop/Data/ApplicationUser.cs
+++ BlazorMotoShop/Data/ApplicationUser.cs
@@ -5,6 +5,7 @@ namespace BlazorMotoShop.Data
     // Add profile data for application users by adding properties to the ApplicationUser class
     public class ApplicationUser : IdentityUser
     {
+        public byte[]? ProfilePicture { get; set; }
     }
 
 }
